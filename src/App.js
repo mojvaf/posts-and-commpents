62be631dfeadd46e9d946b4e99836aff55074a6a
mojvaf <mvafa44@gmail.com>
2020-03-16 10:07:25 +0100

    Fianl changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Post from './component/Post'
+import nena from './images/nena.jpg'
+import img2 from './images/like.png'
+import jack from './images/jack.jpg'
+
+
+const posts = [
+  {
+    "id": 5,
+    "category": "Kultur",
+    "text": "Q: How many programmers does it take to change a light bulb?\nA: None, that's a hardware problem.",
+    "url": null,
+    "image": null,
+    "link_title": null,
+    "link_description": null,
+    "link_source": null,
+    "likes": 1,
+    "liked": true,
+    "created_at": "2020-02-29 19:58:00",
+    "comments": [
+      {
+        "id": "3",
+        "author": {
+          "firstname": "jack",
+          "lastname": "Schmidt",
+          "avatar": jack
+        },
+        "comment": "Haha, good one!",
+        "created_at": "2020-03-01 10:32:12"
+      }
+    ],
+    "author": {
+      "id": 1,
+      "firstname": "nena",
+      "lastname": "",
+      "avatar": nena
+    }
+  },
+  {
+    "id": 4,
+    "category": "Politik",
+    "text": "Who do you think is gonna be the candidate for the Democrats?",
+    "url": "https://www.bbc.com/news/world-us-canada-51638341",
+    "image": "https://ichef.bbci.co.uk/news/384/cpsprodpb/3876/production/_111045441_mediaitem111045440.jpg",
+    "link_title": "Democratic debate: Rivals intensify attacks on Bernie Sanders",
+    "link_description": "The televised showdown before South Carolina and Super Tuesday primaries was a series of messy attacks.",
+    "link_source": "BBC News",
+    "likes": 0,
+    "liked": false,
+    "created_at": "2020-02-27 16:28:31",
+    "comments": [
+      {
+        "id": "3",
+        "author": {
+          "firstname": "jack",
+          "lastname": "Schmidt",
+          "avatar": jack
+        },
+        "comment": "Not sure, could be a close call between Sanders and Biden. Let's see...",
+        "created_at": "2020-02-28 12:19:43"
+      }
+    ],
+    "author": {
+      "id": 1,
+      "firstname": "nena",
+      "lastname": "",
+      "avatar": nena
+    }
+  },
+  {
+    "id": 1,
+    "category": "Politik",
+    "text": "This is a first test. Seems to work fine. Hooray :)",
+    "url": null,
+    "image": null,
+    "link_title": null,
+    "link_description": null,
+    "link_source": null,
+    "likes": 3,
+    "liked": false,
+    "created_at": "2020-02-27 16:27:31",
+    "comments": [],
+    "author": {
+      "id": 1,
+      "firstname": "jack",
+      "lastname": "",
+      "avatar": jack
+    }
+  }
+]
+
+const author = posts.map(i=>i.author)
+const text = posts.map(i=>i.text)
+const created = posts.map(i=>i.created_at)
+const likes = posts.map(i=>i.likes)
+const category= posts.map(i=>i.category)
+const comment = posts.map(i => i.comments)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      <Post props={author} text={text} created={created} comment={comment} likes={likes} category={category}></Post></div>
+
+  )
 }
 
 export default App;
---
+++ src/component/Post.js
@@ -0,0 +1,152 @@
+import React, { Component } from 'react'
+import '../Post.css';
+import nena from '../images/nena.jpg'
+import img2 from '../images/like.png'
+import jack from '../images/jack.jpg'
+
+
+
+class Post extends Component {
+
+    constructor(props) {
+
+        super(props);
+        this.state = {
+            likes: this.props.likes,
+        }
+
+        this.arr = [].concat(this.state.likes)
+        this.innerText = '';
+        this.first = [...this.props.comment.flat()];
+        this.comment = this.first.map(i => i.comment);
+        this.author1 = this.first.map(i => i.author)
+        this.created1 = this.first.map(i => i.created_at)
+
+
+        console.log('text ', this.author2)
+        this.res = this.props.props
+        console.log(this.comment)
+        console.log("--------")
+        console.log(this.author1)
+        console.log(this.created)
+
+        console.log(this.state.likes);
+
+
+    }
+
+
+    textFun = (text) => {
+        if (text.includes('\n')) {
+            console.log("hit", text)
+
+            let QA = text.split('?');
+            // console.log(QA);
+            this.innerText = "innerText"
+            return <div className="innerText"><p>{QA[0] + '?'}</p><p>{QA[1]}</p></div>
+        }
+        else {
+            console.log("hi", text)
+
+            this.innerText = "text"
+            return <div className="text"><p>{text}</p></div>
+        }
+    }
+
+
+    incrementCounter = (index) => {
+        this.arr[index] += 1;
+        this.setState({
+            ...this.state,
+            likes: [...this.arr]
+        })
+    }
+    render() {
+        //let res = this.props.props;
+        let created = this.props.created;
+        let text = this.props.text;
+
+        return (<div className="main">{
+            this.res.map((i, index) =>
+
+                <div className="main1">
+                    <div className="display" key={index}>
+                        <div className="author">
+                            <img src={this.res[index].avatar} className="img"></img>
+                            <div >
+                                <h4 className="names">{this.res[index].firstname + "" + this.res[index].lastname}</h4>
+                                <h6 className="created">{created[index]}</h6>
+                            </div>
+                        </div>
+                        <div >
+
+                            {
+                                this.textFun(text[index])
+                            }
+                        </div>
+
+
+
+                    </div>
+
+                    {//=========Likses Section===============//
+                    }
+
+                    <div className="likesDiv">
+                        <div className="bound">
+                            <img src={img2} className="likeIcon" onClick={() => this.incrementCounter(index)}></img>
+                            <p id="Commentname">Likes : {this.state.likes[index]}</p>
+                            <p id="likesCategory">{this.props.category[index]}</p>
+                        </div>
+                    </div>
+
+
+
+
+
+                    {//-----------commment section--------------------------------
+                    }
+
+                    {(this.comment.length > index
+                        &&
+                        <div className="commentContainer">
+                            <div><p style={{ textAlign: 'left', paddingLeft: '5px' }}><b>Comments :</b></p></div>
+                            <div className="bound">
+
+
+                                <img src={this.author1[index].avatar} className="imgComment"></img>
+
+                                <div className="triangle-left"></div>
+                                <div className="comment">
+
+                                    {(this.author1.length > index) && <p id="Commentname">{this.author1[index].firstname + " " + this.author1[index].lastname} </p>}
+                                    <p className="created1">{this.created1[index]} </p>
+                                    <div style={{ marginTop: '10px' }} > <p>{this.comment[index]} </p></div>
+
+                                </div>
+
+
+                            </div>
+
+                        </div>
+
+
+
+
+
+
+                    )}
+
+                </div>
+
+            )
+        }
+
+        </div>)
+
+
+
+    }
+
+} export default Post
+
